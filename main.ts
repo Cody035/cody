40d46ca8f8b19bff489a84c4eee3333c1ba3ac09
Cody035 <85861279+Cody035@users.noreply.github.com>
2021-06-14 13:51:29 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,4 @@
-input.onButtonPressed(Button.A, function () {
-    判定值 = 1
-})
-input.onButtonPressed(Button.B, function () {
-    判定值 = 2
-})
-function _1casual () {
+function Casual () {
     for (let index = 0; index < 10000; index++) {
         羊狼 = randint(1, 2)
         判定值 = 0
@@ -56,6 +50,64 @@ function _1casual () {
         basic.pause(1000)
     }
 }
-let 羊狼 = 0
+function Insane () {
+	
+}
+function Challenge () {
+	
+}
+input.onButtonPressed(Button.A, function () {
+    判定值 = 1
+})
+function Nightmare () {
+	
+}
+input.onButtonPressed(Button.B, function () {
+    判定值 = 2
+})
 let 判定值 = 0
-_1casual()
+let 羊狼 = 0
+let Start = 0
+for (let index = 0; index < 1000; index++) {
+    Start = 0
+    if (Start == 0) {
+        basic.showNumber(1)
+        basic.pause(1000)
+        if (input.buttonIsPressed(Button.A)) {
+            Casual()
+        }
+        if (input.buttonIsPressed(Button.B)) {
+            Start += 1
+        }
+    }
+    if (Start == 1) {
+        basic.showNumber(2)
+        basic.pause(1000)
+        if (input.buttonIsPressed(Button.A)) {
+            Challenge()
+        }
+        if (input.buttonIsPressed(Button.B)) {
+            Start += 1
+        }
+    }
+    if (Start == 2) {
+        basic.showNumber(3)
+        basic.pause(1000)
+        if (input.buttonIsPressed(Button.A)) {
+            Insane()
+        }
+        if (input.buttonIsPressed(Button.B)) {
+            Start += 1
+        }
+    }
+    if (Start == 3) {
+        basic.showNumber(4)
+        basic.pause(1000)
+        if (input.buttonIsPressed(Button.A)) {
+            Nightmare()
+        }
+        if (input.buttonIsPressed(Button.B)) {
+            Start = 0
+        }
+    }
+}
